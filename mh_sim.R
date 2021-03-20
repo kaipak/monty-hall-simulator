0746def35ad0dcd05033bd6f8d05dd488b950ce1
Kai Pak <kai@kaipak.org>
2021-03-20 00:20:35 -0700

    Complete simulator.

--- mh_sim.R
+++ mh_sim.R
@@ -1,3 +1,4 @@
+library(plotly)
 
 SimulateRound <- function(switch=FALSE) {
   
@@ -27,4 +28,43 @@ SimulateRound <- function(switch=FALSE) {
 SimulateNGames <- function(switch=FALSE, n=10) {
   win.loss <- replicate(n, SimulateRound(switch=switch))
   return(win.loss)
+}
+
+
+VisualizeGames <- function(num_games=10) {
+  wl.hold <- SimulateNGames(switch=FALSE, n=num_games)
+  outcome <- c("win", "loss")
+  record <- c(sum(wl.hold), length(wl.hold) - sum(wl.hold))
+  hold.rec <- data.frame(outcome, record)
+  wl.switch <- SimulateNGames(switch=TRUE, n=num_games)
+  record <- c(sum(wl.switch), length(wl.switch) - sum(wl.switch))
+  switch.rec <- data.frame(outcome, record)
+  
+  print(hold.rec[hold.rec$outcome == 'wins']$record)
+  print(sum(hold.rec$record))
+  fig1 <- plot_ly(
+    domain = list(x = c(0, 0.45), y = c(0, 1)),
+    value = hold.rec[hold.rec$outcome == 'win',]$record / sum(hold.rec$record),
+    title = list(text = "Win Percentage (Hold!)"),
+    type = "indicator",
+    mode = "gauge+number",
+    gauge = list(
+      axis = list(range = list(NULL, 1.0))
+    )) 
+  fig1 <- fig1 %>% layout(margin = list(l=20,r=30))
+  
+  fig2 <- plot_ly(
+    domain = list(x = c(0.55, 1), y = c(0, 1)),
+    value = switch.rec[switch.rec$outcome == 'win',]$record / sum(switch.rec$record),
+    title = list(text = "Win Percentage (Switch!)"),
+    type = "indicator",
+    mode = "gauge+number",
+    gauge = list(
+      axis = list(range = list(NULL, 1.0))
+    )) 
+  fig2 <- fig2 %>% layout(margin = list(l=20,r=30))
+  
+  fig <- subplot(fig1, fig2)
+  
+  return(fig)
 }
